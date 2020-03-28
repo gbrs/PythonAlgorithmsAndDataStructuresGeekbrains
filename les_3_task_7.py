c462df14aec8efa141dc161c8ab66ac3f9f1b9bb
gbrs <gbrs@mail.ru>
2020-03-28 21:37:35 +0300

    3_7 решил задачу

---
+++ les_3_task_7.py
@@ -0,0 +1,26 @@
+'''7. В одномерном массиве целых чисел определить два наименьших элемента.
+Они могут быть как равны между собой (оба минимальны), так и различаться.
+'''
+
+from random import randrange
+
+# a = [6, 6]  # проверка
+# сгенерируем случайную последовательность
+a = [randrange(0, 100) for i in range(10)]
+print(a)
+
+min1 = a[0]
+min2 = a[1]
+
+if min1 > min2:
+    min1, min2 = min2, min1
+
+for i in range(2, len(a)):
+    if a[i] < min2:
+        if a[i] <= min1:
+            min2 = min1
+            min1 = a[i]
+        else:
+            min2 = a[i]
+
+print(min1, min2)

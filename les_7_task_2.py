fbe4d754cb1ef01ed94390c8916c7f6c2967aafb
gbrs <gbrs@mail.ru>
2020-04-15 14:06:39 +0300

    7_2 решил задачу

--- les_7_task_2.py
+++ les_7_task_2.py
@@ -8,12 +8,36 @@ from random import randint
 
 
 def merger(arr):
-    arr.sort()
+    '''
+    Сортировка слиянием. Разделение ~пополам производится "срезами"
+    до матриц с одним элементом. Слияние производится с использованием методов массивов
+    pop, append и extend
+    '''
+    # print('/', arr)  # для контроля процесса разделения
+    if len(arr) < 2:
+        return arr
+
+    # разделяем
+    left = merger(arr[0 : len(arr)//2])
+    right = merger(arr[len(arr)//2 : len(arr)])
+
+    # сливаем
+    total = []
+    while len(left) > 0 and len(right) > 0:
+        total.append(left.pop(0) if left[0] < right[0] else right.pop(0))
+        # print('*', total)  # для контроля процесса слияния
+    else:
+        if len(left) != 0:
+            total.extend(left)
+        else:
+            total.extend(right)
+    # print('*', total)  # для контроля процесса слияния
+    return total
 
 
 size = 10
 a = [randint(0, 49) for i in range(size)]
 print(a)
 
-merger(a)
+a = merger(a)
 print(a)

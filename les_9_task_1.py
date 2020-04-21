4c09081f24526cb63ff57d9e2b5b79f3baf3892a
gbrs <gbrs@mail.ru>
2020-04-22 00:44:16 +0300

    9_1 решил задачу

--- les_9_task_1.py
+++ les_9_task_1.py
@@ -13,18 +13,22 @@ cnt = 0
 
 assert l > 0, 'Молчание, конечно, - золото. Но мне бы фразу подлиннее'
 
+# три вложенных цикла - перебираем: начальный элемент подстроки, ее длину и
+# ищем ее аналоги во всех позициях строки. Запоминаем все подстроки во множество
+# subs и сверяемся с ним, чтобы не считать одни и те же подстроки многократно
 for start in range(l):
     for length in range(1, l-start+1):
         sub = text[start: start + length]
         if sub not in subs:
             for position in range(l-length+1):
                 sample = text[position : position+length]
-                # print(sub, position, sample)
                 if hashlib.sha1(sub.encode('utf-8')).hexdigest() == \
                     hashlib.sha1(sample.encode('utf-8')).hexdigest():
-                    subs.update(sub)
-                    print(sub, position, sample, subs)
-                    break
+                    subs.add(sub)
+                    # print(sub, position, sample, subs)
+                    cnt += 1
 
-print(subs)
-print(len(subs) - 1)
+# удаляем подстроку, совпадающую со всей строкой
+subs.remove(text)
+print('Множество подстрок: ', subs)
+print(f'{len(subs)} различных подстрок, встречающихся {cnt - 1} раз')
